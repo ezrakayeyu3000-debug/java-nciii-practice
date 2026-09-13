720aa236d65b9fecf1a0635fcc5d11e6b5c41a78
ezrakayeyu3000-debug <ezrakayeyu3000@gmail.com>
2026-09-13 19:44:43 +0800

    Changed message on main

--- Practice.java
+++ Practice.java
@@ -1,5 +1,5 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("I'm learning Java NCIII!");
+        System.out.println("This is the MAIN version!");
     }
 }
